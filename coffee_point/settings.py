0be417bb0ed73a501f632120a7087e4eb28a668e
Dmitry Anatsko <anatskodr@gmail.com>
2020-05-21 09:17:55 +0300

    correct settings

--- coffee_point/settings.py
+++ coffee_point/settings.py
@@ -151,19 +151,3 @@ try:
     django_heroku.settings(locals())
 except ImportError:
     pass
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-        },
-    },
-}
