3b7044453499945b71c2909db4cc0e3235eb6303
Dmitry Anatsko <anatskodr@gmail.com>
2020-05-26 23:57:29 +0300

    correct jinja2 settings

--- coffee_point/jinja2.py
+++ coffee_point/jinja2.py
@@ -1,4 +1,6 @@
 from django.contrib.staticfiles.storage import staticfiles_storage
+from django.templatetags.static import static
+
 from django.urls import reverse
 from django.utils import timezone
 from jinja2 import Environment
@@ -7,7 +9,8 @@ from jinja2 import Environment
 def environment(**options):
     env = Environment(**options)
     env.globals.update({
-        'static': staticfiles_storage.url,
+        # 'static': staticfiles_storage.url,
+        'static': static,
         'url': reverse,
         'now': timezone.now
     })
